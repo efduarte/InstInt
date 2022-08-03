929a51dc496cf2090ab6e99cf340aca557cb23a6
Emanuel Felipe Duarte <efduarte@users.noreply.github.com>
2022-08-03 17:04:15 -0300

    Update leds.py

--- leds.py
+++ leds.py
@@ -15,17 +15,20 @@ pixels = neopixel.NeoPixel(
 while True:
     #RED
     pixels.fill((255, 0, 0))
-    time.sleep(1)
+    time.sleep(3)
 
     #GREEN
     pixels.fill((0, 255, 0))
-    time.sleep(1)
+    time.sleep(3)
 
     #BLUE
     pixels.fill((0, 0, 255))
-    time.sleep(1)
+    time.sleep(3)
 
     pixels.fill((0, 0, 0))
+
     for i in range(num_pixels):
         pixels[0] = (255, 255, 255)
         time.sleep(0.01)
+        
+    time.sleep(3)
